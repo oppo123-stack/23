ea3ad3f3151be4ebfeb2c17c808b014e3e92e8e2
ST-009\user <ni4wuder@gmail.com>
2026-02-03 10:16:31 +0800

    新增數據讀取功能，從CSV檔案載入並顯示資料

--- import streamlit as st.py
+++ import streamlit as st.py
@@ -4,3 +4,8 @@ st.title("good morning")
 st.write("This is a simple Streamlit app.")
 name=st.text_input("Enter your name:")
 st.write(f"Hello, {name}!")
+import matplotlib.pyplot as plt
+import pandas as pd
+url="https://raw.githubusercontent.com/GrandmaCan/ML/main/Resgression/Salary_Data.csv"
+data=pd.read_csv(url)
+st.write(data)
